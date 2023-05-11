ba131160adc93491699b8244110cf84916a3bbc0
Fabiano Freitas <fabiano.freitas@gmail.com>
2023-05-11 07:53:56 -0300

    Projeto Ecommerce Flask

---
+++ mercado/__init__.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+
+
+
+# criando o banco de dados
+db = SQLAlchemy()
+login_manager = LoginManager()
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///mercado.db"
+app.config['SECRET_KEY'] = '0d8b405b41e9bd1a3cef5f07'
+db.init_app(app)
+bcrypt = Bcrypt(app)
+login_manager.init_app(app)
+login_manager.login_view = "page_login"
+login_manager.login_message = "Por favor realize o login"
+login_manager.login_message_category = "info"
+
+from mercado import routes
